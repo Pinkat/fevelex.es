6737aae536fcf4790a27cef1fd244f9e5e8bfc36
albertopastormr <general.tyrus@hotmail.com>
2018-07-16 21:45:36 +0200

    Fix contact form

---
+++ php/contactengine.php
@@ -0,0 +1,36 @@
+<?php
+
+$EmailFrom = "chriscoyier@gmail.com";
+$EmailTo = "brokerp18@gmail.es";
+$Subject = "FEVELEX WEBSITE CONTACT FORM";
+$Name = Trim(stripslashes($_POST['first-name'])); 
+$Tel = Trim(stripslashes($_POST['phone'])); 
+$Email = Trim(stripslashes($_POST['email'])); 
+$Message = Trim(stripslashes($_POST['message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Tel: ";
+$Body .= $Tel;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+?>
